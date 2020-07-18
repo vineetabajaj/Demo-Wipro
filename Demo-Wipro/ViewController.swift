e6d97ea6c71c2c359dacde99ffe0ae5fa86713a0
Vineeta <vineeta@mindfiresolutions.com>
2020-07-18 14:06:31 +0530

    Adding Xcode project file

---
+++ Demo-Wipro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo-Wipro
+//
+//  Created by Vineeta on 18/07/20.
+//  Copyright © 2020 Vineeta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
